e736738d856db6666bc8404e14550cf94104923b
HaneulLee <1017oscar@naver.com>
2023-11-03 12:02:23 -0400

    server fix

--- server/server.js
+++ server/server.js
@@ -21,4 +21,4 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.json(__dirname, 'public/assets')));
+// app.use("/assets", express.static(path.json(__dirname, 'public/assets')));
