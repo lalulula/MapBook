73eef4b4cffe1b9a0f1186d60d170df5707caefb
Ratanak-Rin-Sbu <100118148+Ratanak-Rin-Sbu@users.noreply.github.com>
2023-11-04 15:41:01 -0400

    solving failed backend test

--- server/tests/app.test.js
+++ server/tests/app.test.js
@@ -1,10 +1,22 @@
 const request = require("supertest");
+const mongoose = require("mongoose");
 
 const app = require("../app");
 
-jest.useFakeTimers()
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 describe("Test example", () => {
+
+  // Give time to any async operation to complete after each test
+  afterEach(async () => {
+    await sleep(2000);
+  });
+
+  // Also close any pending connection (or related) if any
+  afterAll(() => { 
+    mongoose.connection.close()
+  })
+
   test("POST /api/auth/register", async () => {
     request(app)
       .post("/api/auth/register")
