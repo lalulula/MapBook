343c24292583c2498b5a4becd1c120a41ce1508a
Ratanak-Rin-Sbu <100118148+Ratanak-Rin-Sbu@users.noreply.github.com>
2023-11-04 15:12:37 -0400

    added register auth controller

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-const User = require("./models/User");
+const auth = require("./controllers/auth")
 
 const app = express();
 app.use(express.json());
@@ -9,23 +9,6 @@ app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(cors());
 
-const register = async (req, res) => {
-  try {
-    const {
-    username,
-    password,
-    } = req.body;
-    const newUser = new User({
-    username,
-    password,
-    });
-    const savedUser = await newUser.save();
-    return res.status(201).json(savedUser);
-  } catch (err) {
-    return res.status(500).json({ error: err.message });
-  }
-};
-
-app.post("/api/auth/register", register)
+app.post("/api/auth/register", auth.register)
 
 module.exports = app;
