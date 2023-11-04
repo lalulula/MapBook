6fa29690030515a966c4ef99fcfa1d968718ad17
Yunah Kim <yunah.kim@stonybrook.edu>
2023-11-03 20:22:31 -0400

    Edit Component:Landing

--- client/src/Components/landing/Intro3.js
+++ client/src/Components/landing/Intro3.js
@@ -1,13 +1,21 @@
 import React from "react";
+import "./landingPage.css";
+import Lottie from "lottie-react";
+import Network from "../../assets/Lottie/Network.json";
 
 const Intro3 = () => {
+  const style = {
+    height: 600,
+    width: 600,
+  };
   return (
-    <div>
+    <div className="landing_intro_3">
       <h1>Collaborate and Share</h1>
       <h4>
         Connect with fellow map enthusiasts, and share your maps for the world
         to see.
       </h4>
+      <Lottie animationData={Network} style={style} />
     </div>
   );
 };
