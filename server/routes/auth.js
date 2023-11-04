f170b61523591b8b2c89a7fca1ca39fb76c329dc
Ratanak-Rin-Sbu <100118148+Ratanak-Rin-Sbu@users.noreply.github.com>
2023-11-04 19:15:47 -0400

    added test cases for 2 backend routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,10 @@ const auth = require("../controllers/auth.js")
 
 const router = express.Router();
 
+// CREATE A USER
 router.post("/register", auth.register);
+
+// UPDATE A USER
+router.put("/user", auth.editUser);
 
 module.exports = router
