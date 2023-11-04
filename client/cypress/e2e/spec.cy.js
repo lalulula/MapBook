a486231e002afd2a9e0e31f33e548f69fd1dcb5d
samuelhan713 <samuelhan.s713@gmail.com>
2023-11-04 17:37:19 -0400

    edit frontend testing

--- client/cypress/e2e/spec.cy.js
+++ client/cypress/e2e/spec.cy.js
@@ -5,13 +5,13 @@
 // });
 describe("LandingPage", () => {
   it("renders without errors", () => {
-    cy.visit("http://localhost:3001"); // Adjust the URL as needed
+    cy.visit("http://localhost:3000"); // Adjust the URL as needed
     cy.get(".landing_container").should("exist");
   });
 
   it("displays the correct heading text", () => {
-    cy.visit("http://localhost:3001");
-    cy.get(".landing_right h2").should("contain", "Map Book");
+    cy.visit("http://localhost:3000");
+    cy.get(".landing_right h1").should("contain", "Map Book");
   });
 
   // Add more test cases for interactions, navigation, etc.
