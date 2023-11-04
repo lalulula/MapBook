89c75b0d7484d0213f27bc573ade3ccdd5672131
samuelhan713 <samuelhan.s713@gmail.com>
2023-11-04 18:16:38 -0400

    edit frontend tests

--- client/cypress/e2e/spec.cy.js
+++ client/cypress/e2e/spec.cy.js
@@ -5,12 +5,14 @@
 // });
 describe("LandingPage", () => {
   it("renders without errors", () => {
-    cy.visit("http://localhost:3000"); // Adjust the URL as needed
+    //cy.visit("http://localhost:3000"); // Adjust the URL as needed
+    cy.visit("https://mapbook-f381d1faf354.herokuapp.com/")
     cy.get(".landing_container").should("exist");
   });
 
   it("displays the correct heading text", () => {
-    cy.visit("http://localhost:3000");
+    //cy.visit("http://localhost:3000");
+    cy.visit("https://mapbook-f381d1faf354.herokuapp.com/")
     cy.get(".landing_right h1").should("contain", "Map Book");
   });
 
