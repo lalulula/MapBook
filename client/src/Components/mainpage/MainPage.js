112bbb7bdedc234f65241b7c33eba8e1ef72eac8
samuelhan713 <samuelhan.s713@gmail.com>
2023-11-07 15:34:45 -0500

    fix

--- client/src/Components/mainpage/MainPage.js
+++ client/src/Components/mainpage/MainPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { logout, selectUser } from "../../features/userSlice";
 import { getUserAPIMethod } from "../../api/client";
+import "./main.css";
 
 const MainPage = () => {
   const user = useSelector(selectUser);
@@ -10,11 +11,19 @@ const MainPage = () => {
     dispatch(logout());
   };
 
+  const handleEdit = () => {
+
+  }
+
 
 
   return (
     <div className="main">
-      Hello, {user.username} {/* <button onClick={handleLogout}>Logout, {user.email}</button> */}
+      <div className="main_container">
+        Hello, {user.username} {/* <button onClick={handleLogout}>Logout, {user.email}</button> */}
+      </div>
+      <input></input>
+      <button>update user</button>
     </div>
   );
 };
