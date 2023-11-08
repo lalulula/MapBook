25f68e3db75d8cc020d5cf99734373938d73493a
samuelhan713 <samuelhan.s713@gmail.com>
2023-11-08 14:29:50 -0500

    fix login (without redux)

--- client/cypress/e2e/register.cy.js
+++ client/cypress/e2e/register.cy.js
@@ -1,6 +1,47 @@
 describe("Register Form", () => {
   //CASE1 : When User inputs valid register information
   it("should successfully register a user", () => {
+    cy.visit("http://localhost:3000/register");
+    // Fill out the registration form
+    //cy.get('input[placeholder="Name"]').type("Sam");
+    cy.get('input[placeholder="Username"]').type("TestUser5");
+    //cy.get('input[placeholder="Email"]').type("saaaaam@gmail.com");
+    cy.get('input[placeholder="Password"]').type("Password123!");
+    cy.get('input[placeholder="Confirm Password"]').type("Password123!");
+
+    //  Submit the form
+    cy.get(".register_btn").click();
+
+    // Ensure the registration was successful (you might need to assert the URL or page content)
+    cy.url().should("eq", "http://localhost:3000/login");
+  });
+
+  //CASE2 : When User inputs invalid register information
+  it("should show an error message if passwords don't match", () => {
+    cy.visit("http://localhost:3000/register");
+
+    // Fill out the registration form with passwords that don't match
+    cy.get('input[placeholder="Username"]').type("yolo");
+    cy.get('input[placeholder="Password"]').type("Password123!");
+    cy.get('input[placeholder="Confirm Password"]').type("Pass"); // Passwords don't match
+
+    // Submit the form
+    cy.get(".register_btn").click();
+
+    // Ensure that an error message is displayed
+    cy.get(".ui.negative.mini.message").should(
+      "contain",
+      "Passwords must match"
+    );
+  });
+});
+
+
+// use following code when pushing
+/*
+describe("Register Form", () => {
+  //CASE1 : When User inputs valid register information
+  it("should successfully register a user", () => {
     cy.visit("https://mapbook-f381d1faf354.herokuapp.com/register");
     // Fill out the registration form
     //cy.get('input[placeholder="Name"]').type("Sam");
@@ -31,10 +72,11 @@ describe("Register Form", () => {
     cy.get(".register_btn").click();
 
     // Ensure that an error message is displayed
-    /* cy.get(".ui.negative.mini.message").should(
+    cy.get(".ui.negative.mini.message").should(
       "contain",
       //"Passwords must match"
       "Passwords must be"
-    ); */
+    );
   });
 });
+*/
