0b03fd4e8df300c7eef85dc9311f7ee9f465b889
samuelhan713 <samuelhan.s713@gmail.com>
2023-11-09 21:36:01 -0500

    added comments for maps

---
+++ client/src/Components/mapdetails/MapDetails.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+const MapDetails = () => {
+    const [user, setUser] = useState(null);
+
+    return (
+        <div className="profile">
+
+        </div>
+    )
+}
+
+export default Profile;
