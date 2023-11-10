3ca3f6094d76745011d8e746d6ac7e3359536aad
Yunah Kim <yunah.kim@stonybrook.edu>
2023-11-10 10:18:25 -0500

    edit

--- client/src/Components/searchbar/SearchBar.js
+++ client/src/Components/searchbar/SearchBar.js
@@ -10,7 +10,7 @@ const SearchBar = () => {
         type="search"
         placeholder="Search Maps"
         onChange={(e) => {
-          console.log(e.target.value);
+          console.log(e.target);
         }}
       />
     </div>
