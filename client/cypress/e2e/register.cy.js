b1d93631f1b913105a4664ee0a150f1d6bd64eb1
Yunah Kim <yunah.kim@stonybrook.edu>
2023-11-09 21:28:33 -0500

    Add SearchBar(not Component)

--- client/cypress/e2e/register.cy.js
+++ client/cypress/e2e/register.cy.js
@@ -1,41 +1,40 @@
-describe("Register Form", () => {
-  //CASE1 : When User inputs valid register information
-  it("should successfully register a user", () => {
-    cy.visit("http://localhost:3000/register");
-    // Fill out the registration form
-    //cy.get('input[placeholder="Name"]').type("Sam");
-    cy.get('input[placeholder="Username"]').type("TestUser5");
-    //cy.get('input[placeholder="Email"]').type("saaaaam@gmail.com");
-    cy.get('input[placeholder="Password"]').type("Password123!");
-    cy.get('input[placeholder="Confirm Password"]').type("Password123!");
+// describe("Register Form", () => {
+//   //CASE1 : When User inputs valid register information
+//   it("should successfully register a user", () => {
+//     cy.visit("http://localhost:3000/register");
+//     // Fill out the registration form
+//     //cy.get('input[placeholder="Name"]').type("Sam");
+//     cy.get('input[placeholder="Username"]').type("TestUser5");
+//     //cy.get('input[placeholder="Email"]').type("saaaaam@gmail.com");
+//     cy.get('input[placeholder="Password"]').type("Password123!");
+//     cy.get('input[placeholder="Confirm Password"]').type("Password123!");
 
-    //  Submit the form
-    cy.get(".register_btn").click();
+//     //  Submit the form
+//     cy.get(".register_btn").click();
 
-    // Ensure the registration was successful (you might need to assert the URL or page content)
-    cy.url().should("eq", "http://localhost:3000/login");
-  });
+//     // Ensure the registration was successful (you might need to assert the URL or page content)
+//     cy.url().should("eq", "http://localhost:3000/login");
+//   });
 
-  //CASE2 : When User inputs invalid register information
-  it("should show an error message if passwords don't match", () => {
-    cy.visit("http://localhost:3000/register");
+//   //CASE2 : When User inputs invalid register information
+//   it("should show an error message if passwords don't match", () => {
+//     cy.visit("http://localhost:3000/register");
 
-    // Fill out the registration form with passwords that don't match
-    cy.get('input[placeholder="Username"]').type("yolo");
-    cy.get('input[placeholder="Password"]').type("Password123!");
-    cy.get('input[placeholder="Confirm Password"]').type("Pass"); // Passwords don't match
+//     // Fill out the registration form with passwords that don't match
+//     cy.get('input[placeholder="Username"]').type("yolo");
+//     cy.get('input[placeholder="Password"]').type("Password123!");
+//     cy.get('input[placeholder="Confirm Password"]').type("Pass"); // Passwords don't match
 
-    // Submit the form
-    cy.get(".register_btn").click();
+//     // Submit the form
+//     cy.get(".register_btn").click();
 
-    // Ensure that an error message is displayed
-    cy.get(".ui.negative.mini.message").should(
-      "contain",
-      "Passwords must match"
-    );
-  });
-});
-
+//     // Ensure that an error message is displayed
+//     cy.get(".ui.negative.mini.message").should(
+//       "contain",
+//       "Passwords must match"
+//     );
+//   });
+// });
 
 // use following code when pushing
 /*
