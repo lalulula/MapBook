68dec4cf2e6161ae1e49554f85d1a7a6ef22a9a4
HaneulLee <1017oscar@naver.com>
2023-11-12 17:25:42 -0500

    fix cypress error

--- client/cypress/e2e/landing.cy.js
+++ client/cypress/e2e/landing.cy.js
@@ -1,13 +1,13 @@
 describe("Landing Page", () => {
   it("renders without errors", () => {
     // cy.visit("https://mapbook-f381d1faf354.herokuapp.com/");
-    cy.visit("https://localhost:3000/");
+    cy.visit("http://localhost:3000/");
     cy.get(".landing_container").should("exist");
   });
 
   it("displays the correct heading text", () => {
     // cy.visit("https://mapbook-f381d1faf354.herokuapp.com/");
-    cy.visit("https://localhost:3000/");
+    cy.visit("http://localhost:3000/");
 
     cy.get(".landing_right h1").should("contain", "Map Book");
   });
--- client/cypress/e2e/login.cy.js
+++ client/cypress/e2e/login.cy.js
@@ -2,13 +2,12 @@ describe("Login Page", () => {
   //CASE1 : When User inputs valid login information
   it("should login user successgfully with the correct data", () => {
     // cy.visit("https://localhost:3000/login");
-    cy.visit("https://localhost:3000/login");
+    cy.visit("http://localhost:3000/login");
 
     // Fill out the login form
     /* cy.get('input[placeholder="Username"]').type("Jasson");
     cy.get('input[placeholder="Password"]').type("Jasson123"); */
     cy.get('input[placeholder="Username"]').type("sam");
-    cy.get('input[placeholder="Email"]').type("sam@gmail.com");
 
     cy.get('input[placeholder="Password"]').type("Password123");
     //  Submit the form
@@ -17,16 +16,15 @@ describe("Login Page", () => {
     // Ensure the registration was successful (you might need to assert the URL or page content)
     cy.url().should(
       "eq",
-      "https://localhost:3000/mainpage"
+      "http://localhost:3000/mainpage"
     );
   });
 
   //CASE2 : When User inputs invalid password
   it("should show an error message if password is wrong", () => {
-    cy.visit("https://localhost:3000/login");
+    cy.visit("http://localhost:3000/login");
     // Fill out the login form
     cy.get('input[placeholder="Username"]').type("Jasson");
-    cy.get('input[placeholder="Email"]').type("Jasson@gmail.com");
 
     cy.get('input[placeholder="Password"]').type("Jasson");
     //  Submit the form
