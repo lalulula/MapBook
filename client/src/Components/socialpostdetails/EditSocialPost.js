99c819f30881d971db446a1c59db1a57be5ccb0c
Yunah Kim <yunah.kim@stonybrook.edu>
2023-11-20 18:22:31 -0500

    fix social post preview

--- client/src/Components/socialpostdetails/EditSocialPost.js
+++ client/src/Components/socialpostdetails/EditSocialPost.js
@@ -1,3 +1,73 @@
+// import React, { useState, useEffect } from "react";
+// import { useNavigate, useParams } from "react-router-dom";
+// import {
+//   getSocialPostAPIMethod,
+//   editSocialPostAPIMethod,
+// } from "../../api/social";
+
+// const EditSocialPost = () => {
+//   const { id } = useParams();
+//   const navigate = useNavigate();
+//   const [editedPost, setEditedPost] = useState({});
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const currentPost = await getSocialPostAPIMethod(id);
+//         setEditedPost(currentPost);
+//       } catch (error) {
+//         console.error("Error fetching social post:", error);
+//       }
+//     };
+//     fetchData();
+//   }, [id]);
+
+//   const handleEditSocialPost = async () => {
+//     try {
+//       // Call the API to update the social post
+//       await editSocialPostAPIMethod(id, editedPost);
+
+//       // Navigate back to the Social Post Details page
+//       navigate(`/socialpage`);
+//     } catch (error) {
+//       console.error("Error updating social post:", error);
+//       // Handle error (e.g., display an error message to the user)
+//     }
+//   };
+
+//   const handleChange = (e) => {
+//     // Update the editedPost state when form fields change
+//     setEditedPost({ ...editedPost, [e.target.name]: e.target.value });
+//   };
+
+//   return (
+//     <div>
+//       <h1>Edit Social Post</h1>
+//       <form>
+//         <label>Title:</label>
+//         <input
+//           type="text"
+//           name="title"
+//           value={editedPost.title || ""}
+//           onChange={handleChange}
+//         />
+
+//         <label>Content:</label>
+//         <textarea
+//           name="post_content"
+//           value={editedPost.post_content || ""}
+//           onChange={handleChange}
+//         />
+
+//         <button type="button" onClick={handleEditSocialPost}>
+//           Update Post
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default EditSocialPost;
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import {
@@ -8,7 +78,13 @@ import {
 const EditSocialPost = () => {
   const { id } = useParams();
   const navigate = useNavigate();
-  const [editedPost, setEditedPost] = useState({});
+  const [editedPost, setEditedPost] = useState({
+    title: "",
+    post_content: "",
+    topic: "",
+    customTopic: "",
+    post_images: [],
+  });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -36,7 +112,6 @@ const EditSocialPost = () => {
   };
 
   const handleChange = (e) => {
-    // Update the editedPost state when form fields change
     setEditedPost({ ...editedPost, [e.target.name]: e.target.value });
   };
 
@@ -58,6 +133,25 @@ const EditSocialPost = () => {
           value={editedPost.post_content || ""}
           onChange={handleChange}
         />
+
+        {/* Additional fields based on your form structure */}
+        <label>Topic:</label>
+        <input
+          type="text"
+          name="topic"
+          value={editedPost.topic || ""}
+          onChange={handleChange}
+        />
+
+        <label>Custom Topic:</label>
+        <input
+          type="text"
+          name="customTopic"
+          value={editedPost.customTopic || ""}
+          onChange={handleChange}
+        />
+
+        {/* Handle post_images separately based on your structure */}
 
         <button type="button" onClick={handleEditSocialPost}>
           Update Post
--- client/src/Components/socialpostpreview/SocialPostPreview.js
+++ client/src/Components/socialpostpreview/SocialPostPreview.js
@@ -1,12 +1,59 @@
+// import React, { useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+// import "./socialpostpreview.css";
+// import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+// import CommentIcon from "@mui/icons-material/Comment";
+
+// const SocialPostPreview = ({ data }) => {
+//   const navigate = useNavigate();
+
+//   const handleToSocialDetails = (id) => {
+//     navigate(`/socialpostdetails/${id}`);
+//   };
+//   console.log(data);
+//   useEffect(() => {
+//     console.log(data.post_images);
+//   }, [data]);
+//   return (
+//     <div
+//       className="social_post_preview_container"
+//       onClick={() => handleToSocialDetails(data._id)}
+//     >
+//       <div className="social_post_preview_container_left">
+//         <FavoriteBorderIcon />
+//         {data.social_users_liked.length}
+//       </div>
+//       <div className="social_post_preview_container_middle">
+//         <div className="owner_name">Posted by {data.username}</div>
+//         <div className="social_post_title">
+//           <h3>{data.title}</h3>
+//         </div>
+//         <div className="num_comments">
+//           <CommentIcon />
+//           &nbsp;&nbsp;{data.social_comments.length} comments
+//         </div>
+//       </div>
+//       <div className="social_post_preview_container_right">
+//         <img
+//           className="social_post_preview_img"
+//           alt=""
+//           src={data.post_images[0]}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SocialPostPreview;
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./socialpostpreview.css";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import CommentIcon from "@mui/icons-material/Comment";
-
+import { useSelector } from "react-redux";
 const SocialPostPreview = ({ data }) => {
   const navigate = useNavigate();
-
+  const user = useSelector((state) => state.user.user);
   const handleToSocialDetails = (id) => {
     navigate(`/socialpostdetails/${id}`);
   };
@@ -24,7 +71,7 @@ const SocialPostPreview = ({ data }) => {
         {data.social_users_liked.length}
       </div>
       <div className="social_post_preview_container_middle">
-        <div className="owner_name">Posted by {data.user}</div>
+        <div className="owner_name">Posted by {data.post_owner}</div>
         <div className="social_post_title">
           <h3>{data.title}</h3>
         </div>
