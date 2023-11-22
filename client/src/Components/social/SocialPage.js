71596a92f18e83867990673d8e0c1d09778cc856
Yunah Kim <yunah.kim@stonybrook.edu>
2023-11-21 22:21:12 -0500

    fix social page

--- client/src/Components/social/SocialPage.js
+++ client/src/Components/social/SocialPage.js
@@ -59,39 +59,24 @@ const SocialPage = () => {
       <div className="socialpage_container">
         <div className="socialpage_top">
           <div className="socialpostdetails_top_left">
-            <h1>Social Page</h1>
+            <h2>Social Page</h2>
           </div>
           <div className="socialpage_top_right">
             <div className="create_new_post">
               <Button
+                className="create_post_btn"
                 onClick={() => navigate("/createsocialpost")}
                 variant="outlined"
                 style={{
                   borderColor: "white",
                   color: "white",
                   marginRight: "10px",
+                  width: "max-content",
+                  transition: "transform 0.3s ease",
                 }}
               >
                 Create new post
               </Button>
-
-              <FormControlLabel
-                value="showMySocial"
-                control={
-                  <Checkbox
-                    onChange={handleShowMySocialPosts}
-                    sx={{
-                      color: grey[800],
-                      "&.Mui-checked": {
-                        color: blueGrey[600],
-                      },
-                    }}
-                  />
-                }
-                label="My Posts"
-                labelPlacement="end"
-                color="white"
-              />
             </div>
             <div className="searchbar">
               <SearchBar onSearchChange={(term) => setSearchTerm(term)} />
@@ -108,6 +93,24 @@ const SocialPage = () => {
           </div>
         </div>
         <div className="socialpage_middle">
+          <FormControlLabel
+            style={{ alignSelf: "self-end", marginBottom: "1rem" }}
+            value="showMySocial"
+            control={
+              <Checkbox
+                onChange={handleShowMySocialPosts}
+                sx={{
+                  color: grey[800],
+                  "&.Mui-checked": {
+                    color: blueGrey[600],
+                  },
+                }}
+              />
+            }
+            label="My Posts"
+            labelPlacement="end"
+            color="white"
+          />
           {filteredPosts.map((item, index) => (
             <SocialPostPreview key={index} data={item} />
           ))}
