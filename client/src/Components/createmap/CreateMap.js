3e7f2db1fb69c3c7025aa6d60b1b0aba4ea0f5d5
Yunah Kim <yunah.kim@stonybrook.edu>
2023-11-27 12:42:34 -0500

    Add comments

--- client/src/Components/createmap/CreateMap.js
+++ client/src/Components/createmap/CreateMap.js
@@ -18,10 +18,10 @@ const CreateMap = () => {
     template: "",
     isPrivate: false,
   });
-  const [pieBarData, setPieBarData] = useState([""]); //data names for pie&bar
-  const [themeData, setThemeData] = useState([{ data: "", color: "#fff" }]);
-  const [selectedColors, setSelectedColors] = useState([]);
-  const [heatRange, setHeatRange] = useState({ from: 0, to: 0, width: 0 });
+  const [pieBarData, setPieBarData] = useState([""]); //data names for pie & bar
+  const [themeData, setThemeData] = useState([{ data: "", color: "#fff" }]); //Theme: color and dataname
+  const [selectedColors, setSelectedColors] = useState([]); //HEATMAP: color for each range
+  const [heatRange, setHeatRange] = useState({ from: 0, to: 0, width: 0 }); //HEATMAP: range value
   const [skipStep, setSkipSteps] = useState(false);
   const [importDataOpen, setImportDataOpen] = useState(true);
   const [geojsonData, setGeojsonData] = useState(null);
