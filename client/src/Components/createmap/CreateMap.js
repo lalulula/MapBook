bc7279cbb6eebe9b939678da1077123a17f6142e
Yunah Kim <yunah.kim@stonybrook.edu>
2023-12-01 16:13:55 -0500

    File Drop Box (for Step1:Create Map)

--- client/src/Components/createmap/CreateMap.js
+++ client/src/Components/createmap/CreateMap.js
@@ -1,119 +1,9 @@
-// import React, { useEffect } from "react";
-// import InputMapData from "./InputMapData";
-// import { useState } from "react";
-// import "./createMap.css";
-// import { useNavigate } from "react-router-dom";
-// import Popup from "reactjs-popup";
-// import ImportInitData from "./ImportInitData";
-// import FileDropBox from "./FileDropBox";
-
-// const CreateMap = () => {
-//   const navigate = useNavigate();
-//   const [options, setOptions] = useState({
-//     name: "",
-//     topic: "",
-//     circleHeatMapData: "",
-//     customTopic: "",
-//     template: "",
-//     description: "",
-//     isPrivate: false,
-//   });
-//   const [pieBarData, setPieBarData] = useState([""]); //data names for pie & bar
-//   const [themeData, setThemeData] = useState([{ dataName: "", color: "#fff" }]); //Theme: color and dataname
-//   const [selectedColors, setSelectedColors] = useState([]); //HEATMAP: color for each range
-//   const [heatRange, setHeatRange] = useState({ from: 0, to: 0 }); //HEATMAP: range value
-//   const [skipStep, setSkipSteps] = useState(false);
-//   const [importDataOpen, setImportDataOpen] = useState(true);
-//   const [showMapEdit, setShowMapEdit] = useState(false);
-//   const DEFAULT_GEOJSON =
-//     "https://raw.githubusercontent.com/uber/react-map-gl/master/examples/.data/us-income.geojson";
-
-//   const [selectedMapFile, setSelectedMapFile] = useState(DEFAULT_GEOJSON);
-
-//   useEffect(() => {
-//     // console.log("useEffect: selectedMapFile: ", selectedMapFile);
-//     const newGeojsonData = {
-//       ...selectedMapFile,
-//       mapbook_mapname: options.name,
-//       mapbook_description: options.description,
-//       mapbook_template: options.template,
-//       mapbook_circleheatmapdata: options.circleHeatMapData,
-//       mapbook_topic: options.topic,
-//       mapbook_customtopic: options.customTopic,
-//       mapbook_visibility: options.isPrivate,
-//       mapbook_datanames: pieBarData, //piebar
-//       mapbook_heatrange: heatRange, // heat range
-//       mapbook_heat_selectedcolors: selectedColors, // heat color
-//       mapbook_themedata: themeData, //Color + data name
-//     };
-//     setSelectedMapFile(newGeojsonData);
-//   }, [options, pieBarData, heatRange, selectedColors, themeData]);
-
-//   useEffect(() => {}, [skipStep]);
-
-//   const closeImportDataPopup = () => {
-//     setImportDataOpen(false);
-//   };
-
-//   return (
-//     <div className="createmap_container">
-//       {/* <FileDropBox/> */}
-//       <Popup
-//         open={importDataOpen}
-//         closeOnDocumentClick={false}
-//         closeOnEscape={false}
-//         onClose={closeImportDataPopup}
-//       >
-//         <FileDropBox />
-//         <div>
-//           <ImportInitData
-//             setSkipSteps={setSkipSteps}
-//             setSelectedMapFile={setSelectedMapFile}
-//           />
-//           <div className="modal_btn_container">
-//             <button onClick={() => setImportDataOpen(false)}>
-//               Import Data File
-//             </button>
-//             <button onClick={() => navigate("/mainpage")}>
-//               Return to Main Page
-//             </button>
-//           </div>
-//         </div>
-//       </Popup>
-
-//       {importDataOpen ? null : (
-//         <div className="createmap_add_data_container">
-//           <InputMapData
-//             importDataOpen={importDataOpen}
-//             options={options}
-//             setOptions={setOptions}
-//             pieBarData={pieBarData}
-//             setPieBarData={setPieBarData}
-//             themeData={themeData}
-//             setThemeData={setThemeData}
-//             selectedColors={selectedColors}
-//             setSelectedColors={setSelectedColors}
-//             heatRange={heatRange}
-//             setHeatRange={setHeatRange}
-//             selectedMapFile={selectedMapFile}
-//             setSelectedMapFile={setSelectedMapFile}
-//             showMapEdit={showMapEdit}
-//             setShowMapEdit={setShowMapEdit}
-//           />
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default CreateMap;
 import React, { useEffect } from "react";
 import InputMapData from "./InputMapData";
 import { useState } from "react";
 import "./createMap.css";
 import { useNavigate } from "react-router-dom";
 import Popup from "reactjs-popup";
-import ImportInitData from "./ImportInitData";
 import FileDropBox from "./FileDropBox";
 
 const CreateMap = () => {
@@ -138,6 +28,7 @@ const CreateMap = () => {
     "https://raw.githubusercontent.com/uber/react-map-gl/master/examples/.data/us-income.geojson";
 
   const [selectedMapFile, setSelectedMapFile] = useState(DEFAULT_GEOJSON);
+  // const [selectedMapFile, setSelectedMapFile] = useState(null);
 
   useEffect(() => {
     // console.log("useEffect: selectedMapFile: ", selectedMapFile);
@@ -166,28 +57,19 @@ const CreateMap = () => {
 
   return (
     <div className="createmap_container">
-      {/* <FileDropBox/> */}
       <Popup
         open={importDataOpen}
         closeOnDocumentClick={false}
         closeOnEscape={false}
         onClose={closeImportDataPopup}
       >
-        <FileDropBox />
-        <div>
-          <ImportInitData
-            setSkipSteps={setSkipSteps}
-            setSelectedMapFile={setSelectedMapFile}
-          />
-          <div className="modal_btn_container">
-            <button onClick={() => setImportDataOpen(false)}>
-              Import Data File
-            </button>
-            <button onClick={() => navigate("/mainpage")}>
-              Return to Main Page
-            </button>
-          </div>
-        </div>
+        <FileDropBox
+          setSkipSteps={setSkipSteps}
+          setSelectedMapFile={setSelectedMapFile}
+          selectedMapFile={selectedMapFile}
+          setImportDataOpen={setImportDataOpen}
+        />
+        <div></div>
       </Popup>
 
       {importDataOpen ? null : (
--- client/src/Components/createmap/FileDropBox.js
+++ client/src/Components/createmap/FileDropBox.js
@@ -1,37 +1,124 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import JSZip from "jszip";
+import * as shapefile from "shapefile";
+import { useSelector } from "react-redux";
 import { FileInput, Label } from "flowbite-react";
+import { useNavigate } from "react-router-dom";
 
-function FileDropBox() {
+function FileDropBox({ setSkipSteps, setSelectedMapFile, setImportDataOpen }) {
   const [backgroundColor, setBackgroundColor] = useState("fff");
+  const [textColor, setTextColor] = useState("rgba(128, 128, 128)");
+  const [createTextColor, setCreateTextColor] = useState("rgba(128, 128, 128)");
   const [cursor, setCursor] = useState("auto");
   const [selectedFile, setSelectedFile] = useState(null);
+  const userId = useSelector((state) => state.user.id);
+  const navigate = useNavigate();
 
   const handleDrop = (e) => {
     e.preventDefault();
     setBackgroundColor("#fff");
     setCursor("auto");
-
-    // Handle the dropped files
     const files = e.dataTransfer.files;
-    // Update state with the selected file
-    setSelectedFile(files[0]);
-    // Perform any additional actions with the dropped files
+    setSelectedMapFile(files[0]);
     console.log(files);
   };
 
-  const handleFileChange = (e) => {
-    // Handle the selected file from the input
+  const handleFileChange = async (e) => {
     const file = e.target.files[0];
-    // Update state with the selected file
+
     setSelectedFile(file);
-    // Perform any additional actions with the selected file
-    console.log(file);
+    try {
+      const file = e.target.files[0];
+      const texts = await file.text();
+      let parsedData;
+
+      if (file.name.endsWith(".json") || file.name.endsWith(".geojson")) {
+        parsedData = JSON.parse(texts);
+      } else if (file.name.endsWith(".kml")) {
+        var tj = require("./togeojson");
+        var kml = new DOMParser().parseFromString(texts, "text/xml");
+        parsedData = JSON.parse(JSON.stringify(tj.kml(kml), null, 4));
+      } else if (file.name.endsWith(".zip")) {
+        try {
+          const zip = new JSZip();
+          const zipContents = await zip.loadAsync(file); // Load the ZIP file asynchronously
+          // Find the .shp and .dbf files in the ZIP archive
+          let shpBuffer, dbfBuffer;
+          for (const fileName in zipContents.files) {
+            if (fileName.endsWith(".shp")) {
+              shpBuffer = await zipContents.files[fileName].async(
+                "arraybuffer"
+              );
+            } else if (fileName.endsWith(".dbf")) {
+              dbfBuffer = await zipContents.files[fileName].async(
+                "arraybuffer"
+              );
+            }
+          }
+          // Process shpBuffer and dbfBuffer here
+          // You can use a library like 'shapefile' to read the contents
+
+          const geojson = await shapefile.read(shpBuffer, dbfBuffer);
+          // console.log(geojson.features[0]);
+          for (const data in geojson.features) {
+            var i = 0;
+            var name = "NAME_";
+            for (i = 0; i < 10; i++) {
+              if (geojson.features[data].properties[name + i] === undefined) {
+                i--;
+                break;
+              }
+            }
+
+            geojson.features[data].properties.name =
+              geojson.features[data].properties[name + i];
+          }
+
+          parsedData = geojson;
+        } catch (error) {
+          // Handle any errors that may occur during file processing
+          console.error("Error processing the ZIP file:", error);
+        }
+      }
+      // Check if the "template" key exists at the top level
+      if ("mapbook_template" in parsedData) {
+        setSkipSteps(true);
+        console.log(
+          'The "mapbook_template" key exists with value:',
+          parsedData.mapbook_template
+        );
+        // console.log(parsedData);
+        setSelectedMapFile(parsedData);
+      } else {
+        console.log(
+          'The "mapbook_template" key does not exist at the top level.'
+        );
+        const newGeojsonData = {
+          ...parsedData,
+          mapbook_mapname: "",
+          mapbook_description: "",
+          mapbook_template: "",
+          mapbook_circleheatmapdata: "",
+          mapbook_topic: "",
+          mapbook_customtopic: "",
+          mapbook_visibility: "",
+          mapbook_datanames: [], //piebar
+          mapbook_heatrange: { from: 0, to: 0 }, // heat range
+          mapbook_heat_selectedcolors: [], // heat color
+          mapbook_themedata: [], //Color + data name
+          mapbook_owner: userId,
+        };
+        setSelectedMapFile(newGeojsonData);
+      }
+    } catch (error) {
+      console.error("Error loading GeoJSON file:", error);
+    }
   };
   return (
     <div
       style={{
         border: "1px gray dashed",
-        padding: "4rem",
+        padding: "4rem 4rem 2rem 4rem",
         backgroundColor,
         transition: "background-color 0.2s ease",
         cursor,
@@ -72,7 +159,7 @@ function FileDropBox() {
             <path
               stroke="gray"
               strokeLinecap="round"
-              strokeLineJoin="round"
+              strokeLinejoin="round"
               strokeWidth="2"
               d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
             />
@@ -102,7 +189,7 @@ function FileDropBox() {
               fontSize: "13px",
             }}
           >
-            .geojson .json
+            File types - .geojson .json .kml .shp
           </span>
         </div>
         <FileInput
@@ -121,18 +208,63 @@ function FileDropBox() {
             textAlign: "center",
           }}
         >
-          Selected File:{" "}
+          Selected File :{" "}
           <span
             style={{
-              color: "blue",
+              color: "#1f6baf",
               marginTop: "10px",
               fontWeight: "600",
             }}
           >
             {selectedFile.name}
           </span>
+          <br />
+          <div
+            style={{
+              color: createTextColor,
+              marginTop: "1rem",
+              fontWeight: "600",
+              fontSize: "15px",
+              textAlign: "center",
+              cursor,
+            }}
+            onMouseOver={() => {
+              setCursor("pointer");
+              setCreateTextColor("#1f6baf");
+            }}
+            onMouseOut={() => {
+              setCursor("auto");
+              setCreateTextColor("rgba(128, 128, 128)");
+            }}
+            onClick={() => setImportDataOpen(false)}
+          >
+            Create Map
+          </div>
         </div>
       )}
+
+      <div
+        style={{
+          color: textColor,
+          marginTop: "1rem",
+          fontWeight: "400",
+          fontSize: "10px",
+          textAlign: "center",
+          textDecoration: "underline",
+          cursor,
+        }}
+        onMouseOver={() => {
+          setCursor("pointer");
+          setTextColor("#1f6baf");
+        }}
+        onMouseOut={() => {
+          setCursor("auto");
+          setTextColor("rgba(128, 128, 128)");
+        }}
+        onClick={() => navigate("/mainpage")}
+      >
+        Return to Main Page
+      </div>
     </div>
   );
 }
