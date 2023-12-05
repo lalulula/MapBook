2b6b95213ad0f82e6e3ade77c9c5d5b3bb83e7d3
samuelhan713 <samuelhan.s713@gmail.com>
2023-12-04 22:14:03 -0500

    fixed all cypress tests (besides createmap)

--- client/cypress/e2e/login.cy.js
+++ client/cypress/e2e/login.cy.js
@@ -27,7 +27,7 @@ describe("Login Page", () => {
     //  Submit the form
     cy.get(".login_btn").click();
 
-    cy.get(".invalid_credentials_error_message").should(
+    cy.get(".pwd_err").should(
       "contain",
       "Incorrect username or"
     );
