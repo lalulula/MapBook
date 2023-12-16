bc644b1f689be488924e08934b1caa00161680a6
Ratanak-Rin-Sbu <100118148+Ratanak-Rin-Sbu@users.noreply.github.com>
2023-12-16 15:38:25 -0500

    get rid of Edit option on MapDetails

--- client/src/Components/mapdetails/MapDetails.js
+++ client/src/Components/mapdetails/MapDetails.js
@@ -910,9 +910,6 @@ const MapDetails = () => {
                           Export Map
                         </li>
                         <Divider style={{ margin: "0" }} />
-                        {(isOwner || user.username === "Admin" && (
-                          <li onClick={() => handleEdit()}>Edit Map</li>
-                        ))}
                       </ul>
                     </div>
                   )}
