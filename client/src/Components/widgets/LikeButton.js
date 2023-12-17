1274649bc758c3be0e4d8002aaf9791169ef7179
Yunah Kim <yunah.kim@stonybrook.edu>
2023-12-17 13:45:55 -0500

    Add Like btn in Map details

--- client/src/Components/widgets/LikeButton.js
+++ client/src/Components/widgets/LikeButton.js
@@ -4,25 +4,39 @@ import {
   getSocialPostAPIMethod,
   likeSocialPostAPIMethod,
 } from "../../api/social";
+import { likeMapAPIMethod, getMapAPI } from "../../api/map";
 import "./buttons.css";
 
-function LikeButton({ id, currentPost, isAuth }) {
+function LikeButton({ id, currentPost, isAuth, postType }) {
   const currentUserId = useSelector((state) => state.user.id);
   const [likes, setLikes] = useState(0);
   const [liked, setLiked] = useState(false);
 
   useEffect(() => {
     // Check if currentPost and social_users_liked are defined
-    if (currentPost && currentPost.social_users_liked) {
-      setLikes(currentPost.social_users_liked.length);
-      setLiked(currentPost.social_users_liked.includes(currentUserId));
+    if (postType === "Social") {
+      if (currentPost && currentPost.social_users_liked) {
+        setLikes(currentPost.social_users_liked.length);
+        setLiked(currentPost.social_users_liked.includes(currentUserId));
+      }
+    } else if (postType === "Map") {
+      if (currentPost && currentPost.current.map_users_liked) {
+        setLikes(currentPost.current.map_users_liked.length);
+        setLiked(currentPost.current.map_users_liked.includes(currentUserId));
+      }
     }
   }, [currentPost, currentUserId]);
 
   useEffect(() => {
-    getSocialPostAPIMethod(id).then((post) => {
-      setLikes(post.social_users_liked.length);
-    });
+    if (postType === "Social") {
+      getSocialPostAPIMethod(id).then((post) => {
+        setLikes(post.social_users_liked.length);
+      });
+    } else if (postType === "Map") {
+      getMapAPI(id).then((post) => {
+        setLikes(post.map_users_liked.length);
+      });
+    }
   }, []);
 
   const handleLikeClick = () => {
@@ -30,9 +44,16 @@ function LikeButton({ id, currentPost, isAuth }) {
 
     setLiked((prevLiked) => !prevLiked);
     setLikes((prevLikes) => (liked ? prevLikes - 1 : prevLikes + 1));
-    likeSocialPostAPIMethod(id, userIdJson).then(() =>
-      console.log("liked in social post details!")
-    );
+
+    if (postType === "Social") {
+      likeSocialPostAPIMethod(id, userIdJson).then(() =>
+        console.log("liked in social post details!")
+      );
+    } else if (postType === "Map") {
+      likeMapAPIMethod(id, isAuth, userIdJson).then(() =>
+        console.log("liked in map details!")
+      );
+    }
   };
 
   return (
