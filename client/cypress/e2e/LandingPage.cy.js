7a8271db48f50a7ae883ad478616d0c6cc99e6b2
samuelhan713 <samuelhan.s713@gmail.com>
2023-12-16 23:33:26 -0500

    fixed current cypress tests

--- client/cypress/e2e/LandingPage.cy.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import LandingPage from "../../src/Components/landing/LandingPage";
-
-describe("<LandingPage />", () => {
-  it("renders", () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<LandingPage />);
-  });
-});
