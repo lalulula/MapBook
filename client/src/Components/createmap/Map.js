e3de07d548f81d8cfbeca4f76b6afdb87f324a70
HaneulLee <1017oscar@naver.com>
2023-12-17 12:58:05 -0500

    fix canvas loading

--- client/src/Components/createmap/Map.js
+++ client/src/Components/createmap/Map.js
@@ -102,6 +102,8 @@ const Map = ({
   const navigate = useNavigate();
   const [showErrorMessage, setShowErrorMessage] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [isCanvasLoaded, setIsCanvasLoaded] = useState(false);
+
 
   const handleRerender = () => {
     setRerenderFlag(!rerenderFlag);
@@ -684,39 +686,47 @@ const Map = ({
       /// Haneul
       var expImageSelect = ["case"];
       // generate image object for region which data exist
-      namesDataAdded.forEach((name) => {
-        console.log("name:", name);
-        // generate image
-        // image = generateImage(data);
-        const canvasSave = document.getElementById(name + "pie");
-        console.log("canvasSave:", canvasSave);
-        var context = canvasSave.getContext("2d");
-        console.log("context", context);
-        var imgData = context.getImageData(
-          0,
-          0,
-          canvasSave.width,
-          canvasSave.height
+      try{
+        namesDataAdded.forEach((name) => {
+          console.log("name:", name);
+          // generate image
+          // image = generateImage(data);
+          
+          const canvasSave = document.getElementById(name + "pie");
+          console.log("canvasSave:", canvasSave);
+          var context = canvasSave.getContext("2d");
+          console.log("context", context);
+          var imgData = context.getImageData(
+            0,
+            0,
+            canvasSave.width,
+            canvasSave.height
+          );
+  
+          // add image that we generate
+          if (mapRef.current.hasImage(name)) {
+            mapRef.current.removeImage(name);
+          }
+          mapRef.current.addImage(name, imgData);
+  
+          // add expImageSelect on new image
+          expImageSelect.push(["==", ["get", "name"], name]);
+          expImageSelect.push(name);
+        });
+        //set default image (anything is okay)
+        expImageSelect.push("aaa");
+  
+        mapRef.current.setLayoutProperty(
+          "counties-pie",
+          "icon-image",
+          expImageSelect
         );
-
-        // add image that we generate
-        if (mapRef.current.hasImage(name)) {
-          mapRef.current.removeImage(name);
-        }
-        mapRef.current.addImage(name, imgData);
-
-        // add expImageSelect on new image
-        expImageSelect.push(["==", ["get", "name"], name]);
-        expImageSelect.push(name);
-      });
-      //set default image (anything is okay)
-      expImageSelect.push("aaa");
-
-      mapRef.current.setLayoutProperty(
-        "counties-pie",
-        "icon-image",
-        expImageSelect
-      );
+      }
+      catch (error){
+        // set isCanvasLoaded false
+      }
+      
+      
     } else {
       if (mapRef.current.getLayer("counties-pie")) {
         mapRef.current.setLayoutProperty("counties-pie", "visibility", "none");
@@ -794,39 +804,45 @@ const Map = ({
       /// Haneul
       var expImageSelect = ["case"];
       // generate image object for region which data exist
-      namesDataAdded.forEach((name) => {
-        console.log("name:", name);
-        // generate image
-        // image = generateImage(data);
-        const canvasSave = document.getElementById(name + "bar");
-        console.log("canvasSave:", canvasSave);
-
-        var context = canvasSave.getContext("2d");
-        var imgData = context.getImageData(
-          0,
-          0,
-          canvasSave.width,
-          canvasSave.height
+      try{
+        namesDataAdded.forEach((name) => {
+          console.log("name:", name);
+          // generate image
+          // image = generateImage(data);
+          const canvasSave = document.getElementById(name + "bar");
+          console.log("canvasSave:", canvasSave);
+  
+          var context = canvasSave.getContext("2d");
+          var imgData = context.getImageData(
+            0,
+            0,
+            canvasSave.width,
+            canvasSave.height
+          );
+  
+          // add image that we generate
+          if (mapRef.current.hasImage(name)) {
+            mapRef.current.removeImage(name);
+          }
+          mapRef.current.addImage(name, imgData);
+  
+          // add expImageSelect on new image
+          expImageSelect.push(["==", ["get", "name"], name]);
+          expImageSelect.push(name);
+        });
+        //set default image (anything is okay)
+        expImageSelect.push("aaa");
+  
+        mapRef.current.setLayoutProperty(
+          "counties-bar",
+          "icon-image",
+          expImageSelect
         );
+      }
+      catch(error){
 
-        // add image that we generate
-        if (mapRef.current.hasImage(name)) {
-          mapRef.current.removeImage(name);
-        }
-        mapRef.current.addImage(name, imgData);
-
-        // add expImageSelect on new image
-        expImageSelect.push(["==", ["get", "name"], name]);
-        expImageSelect.push(name);
-      });
-      //set default image (anything is okay)
-      expImageSelect.push("aaa");
-
-      mapRef.current.setLayoutProperty(
-        "counties-bar",
-        "icon-image",
-        expImageSelect
-      );
+      }
+      
     } else {
       if (mapRef.current.getLayer("counties-bar")) {
         mapRef.current.setLayoutProperty("counties-bar", "visibility", "none");
