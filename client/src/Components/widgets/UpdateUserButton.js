a51801adfa9313e28caca18d1fbfbde83b3f805e
Yunah Kim <yunah.kim@stonybrook.edu>
2023-12-17 21:49:44 -0500

    fix css buttons

--- client/src/Components/widgets/UpdateUserButton.js
+++ client/src/Components/widgets/UpdateUserButton.js
@@ -11,12 +11,30 @@ const UpdateUserButton = ({ onClick, text }) => {
           : "update_user_btn_container"
       }
     >
-      <Button
+      {/* <Button
         onClick={onClick}
         style={{ marginTop: "20px" }}
         content={text}
         primary
-      />
+      /> */}
+      <button
+        className="profile_update_cancel_btn"
+        onClick={onClick}
+        style={{
+          marginTop: "20px",
+          outline: "none",
+          backgroundColor: "transparent",
+          fontSize: "15px",
+          fontWeight: "bold",
+          color: "#333",
+          cursor: "pointer",
+          transition: "all 0.2s ease-in-out",
+          padding: " 5px 10px",
+          border: "2px solid #333",
+        }}
+      >
+        {text}
+      </button>
     </div>
   );
 };
